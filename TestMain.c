24ba9d8ea22792a7dad4599c38d88a3a560feef9
Daniel Nogueira <daniel.nogueira.dacosta@gmail.com>
2023-02-10 16:51:06 -0300

    Incuindo o test de passagem

--- TestMain.c
+++ TestMain.c
@@ -1,8 +1,11 @@
 #include "TestOnibus.h"
 #include "TestPassageiro.h"
+#include "TestPassagem.h"
 
 int main() {
     agrupaTestesOnibus();
     agrupaTestesPassageiro();
+    agrupaTestesPassagem();
+
     return 0;
 }
