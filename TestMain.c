69aed2b6d96b19bd6a19caf0bf012c466e9218cd
Daniel Nogueira <daniel.nogueira.dacosta@gmail.com>
2023-02-10 14:33:29 -0300

    Estrutura de testes para Passageiro.c

--- TestMain.c
+++ TestMain.c
@@ -1,7 +1,8 @@
 #include "TestOnibus.h"
+#include "TestPassageiro.h"
 
 int main() {
     agrupaTestesOnibus();
-
+    agrupaTestesPassageiro();
     return 0;
 }
---
+++ TestPassageiro.c
@@ -0,0 +1,37 @@
+#include "Passageiro.h"
+#include <stdio.h>
+#include <string.h>
+
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_DEFAULT "\033[0m"
+#define ANSI_COLOR_GREEN "\x1b[32m"
+
+void testIniciaPassageiroAtributos() {
+    char *nome = "Joao";
+    char *cpf = "12345678910";
+    char *telefone = "10987654321";
+    char *email = "joao@gmail.com";
+    char *telefoneDeEmergencia = "01020304050";
+
+    Passageiro *passageiro = iniciaPassageiro(nome, cpf, telefone, email, telefoneDeEmergencia);
+
+    if (strcmp(getNome(passageiro), nome) == 0) {
+        if (strcmp(getCpf(passageiro), cpf) == 0) {
+            if (strcmp(getTelefone(passageiro), telefone) == 0) {
+                if (strcmp(getEmail(passageiro), email) == 0) {
+                    if (strcmp(getTelefoneDeEmergencia(passageiro), telefoneDeEmergencia) == 0) {
+                        printf(ANSI_COLOR_GREEN "testIniciaPassageiroAtributos(): Success!\n" ANSI_DEFAULT);
+                        liberaPassageiro(passageiro);
+                        return;
+                    }
+                }
+            }
+        }
+    } else {
+        printf(ANSI_COLOR_RED "testIniciaPassageiroAtributos(): Failed!\n" ANSI_DEFAULT);
+    }
+}
+
+void agrupaTestesPassageiro() {
+    testIniciaPassageiroAtributos();
+}
